0f8d2e51a8648b9268a247215da7c153208c3dc4
Sam <sammeown@gmail.com>
2021-10-20 00:10:55 +0300

    Fixed code style

--- main.py
+++ main.py
@@ -121,7 +121,7 @@ def draw(canvas):
                 coroutine.send(None)
             except StopIteration:
                 coroutines.remove(coroutine)
-        if len(coroutines) == 0:
+        if not coroutines:
             break
         canvas.refresh()
         time.sleep(TIC_TIMEOUT)
